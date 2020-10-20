f45e4cd0c659ce59760a39e966586dc6b2720253
cedrict <c.thieulot@uu.nl>
2020-10-20 12:40:57 +0200

    f88updt

--- python_codes/fieldstone_88/stone.py
+++ python_codes/fieldstone_88/stone.py
@@ -6,6 +6,34 @@ import scipy.sparse as sps
 from scipy.sparse.linalg.dsolve import linsolve
 from scipy.sparse import lil_matrix
 import time as timing
+import random
+from scipy import special
+
+#------------------------------------------------------------------------------
+
+def interpolate_vel_on_pt(xm,ym):
+    ielx=int(xm/Lx*nelx)
+    iely=int(ym/Ly*nely)
+    #if ielx<0:
+    #   exit('ielx<0')
+    #if iely<0:
+    #   exit('iely<0')
+    #if ielx>=nelx:
+    #   exit('ielx>nelx')
+    #if iely>=nely:
+    #   exit('iely>nely')
+    iel=nelx*(iely)+ielx
+    xmin=xV[iconV[0,iel]] ; xmax=xV[iconV[8,iel]]
+    ymin=yV[iconV[0,iel]] ; ymax=yV[iconV[8,iel]]
+    rm=((xm-xmin)/(xmax-xmin)-0.5)*2
+    sm=((ym-ymin)/(ymax-ymin)-0.5)*2
+    NNNV[0:mV]=NNV(rm,sm,order)
+    um=0.
+    vm=0.
+    for k in range(0,mV):
+        um+=NNNV[k]*u[iconV[k,iel]]
+        vm+=NNNV[k]*v[iconV[k,iel]]
+    return um,vm,rm,sm,iel 
 
 #------------------------------------------------------------------------------
 # viscosity function
@@ -14,26 +42,51 @@ import time as timing
 def eta(T,e,y):
     TT=min(T,1)
     TT=max(T,0)
-    if y <= 0.77:
-       val=50*np.exp(-6.9*TT) # lower mantle
-       rh=1
-    elif y <= 0.9:
-       val=0.8*np.exp(-6.9*TT) # upper mantle
-       rh=1
-    else:
-       eta_v=10*np.exp(-6.9*TT)
-       eta_p=sigma_y/2/e
-       if eta_v<eta_p:
-          val=eta_v
+    if viscosity_model==1: # bugg08
+       if y <= 0.77:
+          val=50*np.exp(-6.9*TT) # lower mantle
+          rh=1
+       elif y <= 0.9:
+          val=0.8*np.exp(-6.9*TT) # upper mantle
           rh=1
        else:
-          val=eta_p
-          rh=2
+          eta_v=10*np.exp(-6.9*TT)
+          eta_p=sigma_y/2/e
+          if eta_v<eta_p:
+             val=eta_v
+             rh=1
+          else:
+             val=eta_p
+             rh=2
+    if viscosity_model==2: # brhv08
+       bcoeff=np.log(1000)
+       rh=0
+       if y>0.9551:
+          val=1000
+       elif y>0.7187:
+          val=np.exp(-bcoeff*TT)
+       else:
+          val=30*np.exp(-bcoeff*TT)
+    if viscosity_model==3: # budt14
+       rh=0
+       if y>0.9585:
+          val=np.exp(9.2103*(0.5-TT))
+       elif y>0.7682:
+          val=1./30.*np.exp(9.2103*(0.5-TT))
+       else:
+          val=(-6.24837*y + 6.8)*np.exp(9.2103*(0.5-TT))
+
     return val,rh
 
 #------------------------------------------------------------------------------
 # velocity shape functions
 #------------------------------------------------------------------------------
+# Q2          Q1
+# 6---7---8   2-------3
+# |       |   |       |
+# 3   4   5   |       |    etc ...
+# |       |   |       |
+# 0---1---2   0-------1
 
 def NNV(r,s,order):
     if order==1:
@@ -380,23 +433,26 @@ if int(len(sys.argv) == 7):
    Ra    = float(sys.argv[5])
    nstep = int(sys.argv[6])
 else:
-   nelx = 128
+   nelx = 64
    nely = 32
    visu = 1
    order= 2
    Ra = 1e6
    nstep=1000
 
-#for snow still
-hcond=1
+nmarker_per_dim=4 
+random_markers=1
+RKorder=1 # cheap!
 
+viscosity_model=3
 sigma_y=1.5e5
-tfinal=1.5e-4
+tfinal=1.5e-2
 
 CFL_nb=0.5
-apply_filter=True
+apply_filter=False
+supg_type=1
 
-every=10
+every=1
 
 #tol_ss=1e-6   # tolerance for steady state 
 
@@ -449,9 +505,11 @@ NfemT=NV*ndofT       # nb of temperature dofs
 hx=Lx/nelx # element size in x direction
 hy=Ly/nely # element size in y direction
 
-sparse=False # storage of FEM matrix 
+sparse=True # storage of FEM matrix 
 
 #################################################################
+
+sqrt2=np.sqrt(2)
 
 nqperdim=order+1
 
@@ -498,11 +556,13 @@ if nqperdim==6:
 #################################################################
 # open output files
 
-Nu_vrms_file=open('Nu_vrms.ascii',"w")
-Nu_vrms_file.write("#time,Nusselt,vrms\n")
+vrms_file=open('vrms.ascii',"w")
 dt_file=open('dt.ascii',"w")
 Tavrg_file=open('Tavrg.ascii',"w")
 conv_file=open('conv.ascii',"w")
+Tstats_file=open('stats_T.ascii',"w")
+ustats_file=open('stats_u.ascii',"w")
+vstats_file=open('stats_v.ascii',"w")
 
 #################################################################
 
@@ -704,14 +764,24 @@ T = np.zeros(NV,dtype=np.float64)
 T_prev = np.zeros(NV,dtype=np.float64)
 
 for i in range(0,NV):
-    T[i]=1.-yV[i]-0.03*(np.cos(2.132*np.pi*xV[i]/Lx)+\
-                        np.cos(3.333*np.pi*xV[i]/Lx)+\
-                        np.cos(7.123*np.pi*xV[i]/Lx)) *np.sin(np.pi*yV[i]/Ly)
+
+    #T[i]=1.-yV[i]  # conductive profile
+
+    T[i]=0.5
+
+    kappa=1
+    T[i]-=0.5*special.erfc((Ly-yV[i])/2/np.sqrt(kappa*0.001))
+    T[i]+=0.5*special.erfc(yV[i]/2/np.sqrt(kappa*0.001))
+
+    T[i]-=0.03*(np.cos(2.132*np.pi*xV[i]/Lx)+\
+                np.cos(3.333*np.pi*xV[i]/Lx)+\
+                np.cos(7.123*np.pi*xV[i]/Lx)) *np.sin(np.pi*yV[i]/Ly)
+
 #end for
 
 T_prev[:]=T[:]
 
-#np.savetxt('temperature_init.ascii',np.array([x,y,T]).T,header='# x,y,T')
+np.savetxt('temperature_init.ascii',np.array([xV,yV,T]).T,header='# x,y,T')
 
 #################################################################
 # compute area of elements
@@ -748,6 +818,87 @@ print("     -> total area %.6f " %(area.sum()))
 
 print("compute elements areas: %.3f s" % (timing.time() - start))
 
+#################################################################
+# marker setup
+#################################################################
+start = timing.time()
+
+nmarker_per_element=nmarker_per_dim**2
+nmarker=nel*nmarker_per_element
+
+swarm_x=np.empty(nmarker,dtype=np.float64)  
+swarm_y=np.empty(nmarker,dtype=np.float64)  
+swarm_u=np.zeros(nmarker,dtype=np.float64)  
+swarm_v=np.zeros(nmarker,dtype=np.float64)  
+swarm_active=np.zeros(nmarker,dtype=np.bool) 
+
+if random_markers==1:
+   counter=0
+   for iel in range(0,nel):
+       x1=xV[iconV[0,iel]] ; y1=yV[iconV[0,iel]]
+       x2=xV[iconV[2,iel]] ; y2=yV[iconV[2,iel]]
+       x3=xV[iconV[8,iel]] ; y3=yV[iconV[8,iel]]
+       x4=xV[iconV[6,iel]] ; y4=yV[iconV[6,iel]]
+       for im in range(0,nmarker_per_element):
+           # generate random numbers r,s between 0 and 1
+           r=random.uniform(-1.,+1)
+           s=random.uniform(-1.,+1)
+           N1=0.25*(1-r)*(1-s)
+           N2=0.25*(1+r)*(1-s)
+           N3=0.25*(1+r)*(1+s)
+           N4=0.25*(1-r)*(1+s)
+           swarm_x[counter]=N1*x1+N2*x2+N3*x3+N4*x4
+           swarm_y[counter]=N1*y1+N2*y2+N3*y3+N4*y4
+           counter+=1
+
+else:
+   counter=0
+   for iel in range(0,nel):
+       x1=xV[iconV[0,iel]] ; y1=yV[iconV[0,iel]]
+       x2=xV[iconV[2,iel]] ; y2=yV[iconV[2,iel]]
+       x3=xV[iconV[8,iel]] ; y3=yV[iconV[8,iel]]
+       x4=xV[iconV[6,iel]] ; y4=yV[iconV[6,iel]]
+       for j in range(0,nmarker_per_dim):
+           for i in range(0,nmarker_per_dim):
+               r=-1.+i*2./nmarker_per_dim + 1./nmarker_per_dim
+               s=-1.+j*2./nmarker_per_dim + 1./nmarker_per_dim
+               N1=0.25*(1-r)*(1-s)
+               N2=0.25*(1+r)*(1-s)
+               N3=0.25*(1+r)*(1+s)
+               N4=0.25*(1-r)*(1+s)
+               swarm_x[counter]=N1*x1+N2*x2+N3*x3+N4*x4
+               swarm_y[counter]=N1*y1+N2*y2+N3*y3+N4*y4
+               counter+=1
+
+swarm_active[:]=True
+
+print("     -> nmarker %d " % nmarker)
+print("     -> swarm_x (m,M) %.4f %.4f " %(np.min(swarm_x),np.max(swarm_x)))
+print("     -> swarm_y (m,M) %.4f %.4f " %(np.min(swarm_y),np.max(swarm_y)))
+
+print("markers setup: %.3f s" % (timing.time() - start))
+
+#################################################################
+# marker paint
+#################################################################
+start = timing.time()
+
+swarm_mat=np.zeros(nmarker,dtype=np.int32)  
+
+for i in [0,2,4,6,8,10,12,14]:
+    dx=Lx/16
+    for im in range (0,nmarker):
+        if swarm_x[im]>i*dx and swarm_x[im]<(i+1)*dx:
+           swarm_mat[im]+=1
+
+for i in [0,2]:
+    dy=Ly/4
+    for im in range (0,nmarker):
+        if swarm_y[im]>i*dy and swarm_y[im]<(i+1)*dy:
+           swarm_mat[im]+=1
+
+print("markers paint: %.3f s" % (timing.time() - start))
+
 #==============================================================================
 #==============================================================================
 #==============================================================================
@@ -770,8 +921,6 @@ c_mat   = np.array([[2,0,0],[0,2,0],[0,0,1]],dtype=np.float64)
 exx_n   = np.zeros(NV,dtype=np.float64)  
 eyy_n   = np.zeros(NV,dtype=np.float64)  
 exy_n   = np.zeros(NV,dtype=np.float64)  
-
-Nusselt_prev=1.
 
 time=0
 
@@ -994,6 +1143,11 @@ for istep in range(0,nstep):
     print("     -> v (m,M) %.4f %.4f " %(np.min(v),np.max(v)))
     print("     -> p (m,M) %.4f %.4f " %(np.min(p),np.max(p)))
 
+    ustats_file.write("%6e %6e %6e\n" % (time,np.min(u),np.max(u)))
+    vstats_file.write("%6e %6e %6e\n" % (time,np.min(v),np.max(v)))
+    ustats_file.flush()
+    vstats_file.flush()
+
     #np.savetxt('velocity.ascii',np.array([xV,yV,u,v]).T,header='# x,y,u,v')
     #np.savetxt('pressure.ascii',np.array([xP,yP,p]).T,header='# x,y,p')
 
@@ -1028,7 +1182,10 @@ for istep in range(0,nstep):
     rhs   = np.zeros(NfemT,dtype=np.float64)         # FE rhs 
     B_mat=np.zeros((2,ndofT*mV),dtype=np.float64)     # gradient matrix B 
     N_mat = np.zeros((mV,1),dtype=np.float64)         # shape functions
+    N_mat_supg = np.zeros((mV,1),dtype=np.float64)         # shape functions
+    tau_supg = np.zeros(nel*nqperdim**ndim,dtype=np.float64)
 
+    counterq=0   
     for iel in range (0,nel):
 
         b_el=np.zeros(mV*ndofT,dtype=np.float64)
@@ -1075,14 +1232,27 @@ for istep in range(0,nstep):
                     B_mat[1,k]=dNNNVdy[k]
                 #end for
 
+                if supg_type==0:
+                   tau_supg[counterq]=0.
+                elif supg_type==1:
+                      tau_supg[counterq]=(hx*sqrt2)/2/order/np.sqrt(vel[0,0]**2+vel[0,1]**2)
+                elif supg_type==2:
+                      tau_supg[counterq]=(hx*sqrt2)/order/np.sqrt(vel[0,0]**2+vel[0,1]**2)/sqrt15
+                else:
+                   exit("supg_type: wrong value")
+    
+                N_mat_supg=N_mat+tau_supg[counterq]*np.transpose(vel.dot(B_mat))
+
                 # compute mass matrix
-                MM+=N_mat.dot(N_mat.T)*weightq*jcob
+                MM+=N_mat_supg.dot(N_mat.T)*weightq*jcob
 
                 # compute diffusion matrix
                 Kd+=B_mat.T.dot(B_mat)*weightq*jcob
 
                 # compute advection matrix
-                Ka+=N_mat.dot(vel.dot(B_mat))*weightq*jcob
+                Ka+=N_mat_supg.dot(vel.dot(B_mat))*weightq*jcob
+
+                counterq+=1
 
             #end for
         #end for
@@ -1119,6 +1289,8 @@ for istep in range(0,nstep):
 
     #end for iel
 
+    print("     -> tau_supg (m,M) %e %e " %(np.min(tau_supg),np.max(tau_supg)))
+
     print("build FE matrix : %.3f s" % (timing.time() - start))
 
     #################################################################
@@ -1129,6 +1301,9 @@ for istep in range(0,nstep):
     Traw = sps.linalg.spsolve(sps.csr_matrix(A_mat),rhs)
 
     print("     T (m,M) %.4f %.4f " %(np.min(Traw),np.max(Traw)))
+
+    Tstats_file.write("%6e %6e %6e\n" % (time,np.min(Traw),np.max(Traw)))
+    Tstats_file.flush()
 
     print("solve T time: %.3f s" % (timing.time() - start))
 
@@ -1231,6 +1406,9 @@ for istep in range(0,nstep):
     Tavrg_file.write("%10e %10e\n" % (time,Tavrg))
     Tavrg_file.flush()
 
+    vrms_file.write("%10e %.10f\n" % (time,vrms))
+    vrms_file.flush()
+
     print("     istep= %.6d ; vrms   = %.6f" %(istep,vrms))
 
     print("compute vrms: %.3f s" % (timing.time() - start))
@@ -1247,8 +1425,6 @@ for istep in range(0,nstep):
     eta_n = np.zeros(NV,dtype=np.float64)  
     rh_n  = np.zeros(NV,dtype=np.float64)  
     count = np.zeros(NV,dtype=np.int32)  
-    qx_n = np.zeros(NV,dtype=np.float64)  
-    qy_n = np.zeros(NV,dtype=np.float64)  
     q=np.zeros(NV,dtype=np.float64)
     c=np.zeros(NV,dtype=np.float64)
 
@@ -1275,21 +1451,15 @@ for istep in range(0,nstep):
             e_xx=0.
             e_yy=0.
             e_xy=0.
-            q_x=0.
-            q_y=0.
             for k in range(0,mV):
                 e_xx += dNNNVdx[k]*u[iconV[k,iel]]
                 e_yy += dNNNVdy[k]*v[iconV[k,iel]]
                 e_xy += 0.5*(dNNNVdy[k]*u[iconV[k,iel]]+dNNNVdx[k]*v[iconV[k,iel]])
-                q_x -= hcond*dNNNVdx[k]*T[iconV[k,iel]]
-                q_y -= hcond*dNNNVdy[k]*T[iconV[k,iel]]
             #end for
             inode=iconV[i,iel]
             exx_n[inode]+=e_xx
             eyy_n[inode]+=e_yy
             exy_n[inode]+=e_xy
-            qx_n[inode]+=q_x
-            qy_n[inode]+=q_y
             q[inode]+=np.dot(p[iconP[0:mP,iel]],NNNP[0:mP])
             count[inode]+=1
         #end for
@@ -1298,8 +1468,6 @@ for istep in range(0,nstep):
     exx_n/=count
     eyy_n/=count
     exy_n/=count
-    qx_n/=count
-    qy_n/=count
     q/=count
 
     sr_n=np.sqrt(0.5*(exx_n**2+eyy_n**2)+exy_n**2)
@@ -1312,8 +1480,6 @@ for istep in range(0,nstep):
     print("     -> eyy_n (m,M) %.6e %.6e " %(np.min(eyy_n),np.max(eyy_n)))
     print("     -> exy_n (m,M) %.6e %.6e " %(np.min(exy_n),np.max(exy_n)))
     print("     -> sr_n (m,M) %.6e %.6e " %(np.min(sr_n),np.max(sr_n)))
-    print("     -> qx_n (m,M) %.6e %.6e " %(np.min(qx_n),np.max(qx_n)))
-    print("     -> qy_n (m,M) %.6e %.6e " %(np.min(qy_n),np.max(qy_n)))
     print("     -> eta_n (m,M) %.6e %.6e " %(np.min(eta_n),np.max(eta_n)))
 
     #np.savetxt('q.ascii',np.array([xV,yV,q]).T,header='# x,y,q')
@@ -1321,40 +1487,7 @@ for istep in range(0,nstep):
 
     print("compute press & sr: %.3f s" % (timing.time() - start))
 
-    #################################################################
-    # compute Nusselt number at top
-    #################################################################
-    start = timing.time()
 
-    Nusselt=0
-    for iel in range(0,nel):
-        if yV[iconV[mV-1,iel]]>1-eps: 
-           for iq in range(0,nqperdim):
-               rq=qcoords[iq]
-               sq=+1.
-               weightq=qweights[iq]
-               NNNV[0:mV]=NNV(rq,sq,order)
-               xq=0.
-               q_y=0.
-               for k in range(0,mV):
-                   xq += NNNV[k]*xV[iconV[k,iel]]
-                   q_y -= NNNV[k]*qy_n[iconV[k,iel]]
-               #end for
-               jcob=hx/2.
-               Nusselt+=q_y*jcob*weightq
-               #print (xq,q_y)
-           #end for
-        #end if
-    #end for
-
-    Nusselt=np.abs(Nusselt)/Lx
-
-    Nu_vrms_file.write("%10e %.10f %.10f\n" % (time,Nusselt,vrms))
-    Nu_vrms_file.flush()
-
-    print("     istep= %d ; Nusselt= %e ; Ra= %e " %(istep,Nusselt,Ra))
-
-    print("compute Nu: %.3f s" % (timing.time() - start))
 
     #####################################################################
     # compute temperature & visc profile
@@ -1392,6 +1525,28 @@ for istep in range(0,nstep):
     print("compute profiles: %.3f s" % (timing.time() - start))
 
     #####################################################################
+    # advect markers
+    #####################################################################
+    start = timing.time()
+
+    if RKorder==1: 
+
+       for im in range(0,nmarker):
+           if swarm_active[im]:
+              swarm_u[im],swarm_v[im],rm,sm,iel =interpolate_vel_on_pt(swarm_x[im],swarm_y[im])
+              swarm_x[im]+=swarm_u[im]*dt
+              swarm_y[im]+=swarm_v[im]*dt
+              if swarm_x[im]<0 or swarm_x[im]>Lx or swarm_y[im]<0 or swarm_y[im]>Ly:
+                 swarm_active[im]=False
+                 swarm_x[im]=-0.0123
+                 swarm_y[im]=-0.0123
+           # end if active
+       # end for im
+
+
+    print("advect markers: %.3f s" % (timing.time() - start))
+
+    #####################################################################
     # plot of solution
     #####################################################################
     start = timing.time()
@@ -1427,7 +1582,7 @@ for istep in range(0,nstep):
            vtufile.write("%10e \n" %T[i])
        vtufile.write("</DataArray>\n")
        #--
-       vtufile.write("<DataArray type='Float32' Name='eta' Format='ascii'> \n")
+       vtufile.write("<DataArray type='Float32' Name='viscosity' Format='ascii'> \n")
        for i in range(0,NV):
            vtufile.write("%10e \n" %eta_n[i])
        vtufile.write("</DataArray>\n")
@@ -1463,11 +1618,6 @@ for istep in range(0,nstep):
            vtufile.write("%10e \n" %(2*eta_n[i]*sr_n[i]))
        vtufile.write("</DataArray>\n")
        #--
-       vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Name='heat flux' Format='ascii'> \n")
-       for i in range(0,NV):
-           vtufile.write("%10e %10e %10e \n" %(qx_n[i],qy_n[i],0.))
-       vtufile.write("</DataArray>\n")
-       #--
        vtufile.write("</PointData>\n")
        #####
        vtufile.write("<Cells>\n")
@@ -1485,6 +1635,66 @@ for istep in range(0,nstep):
        vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
        for iel in range (0,nel2):
            vtufile.write("%d \n" %9)
+       vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("</Cells>\n")
+       #####
+       vtufile.write("</Piece>\n")
+       vtufile.write("</UnstructuredGrid>\n")
+       vtufile.write("</VTKFile>\n")
+       vtufile.close()
+
+       filename = 'markers_{:04d}.vtu'.format(istep) 
+       vtufile=open(filename,"w")
+       vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+       vtufile.write("<UnstructuredGrid> \n")
+       vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(nmarker,nmarker))
+       #####
+       vtufile.write("<Points> \n")
+       #--
+       vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+       for im in range(0,nmarker):
+           vtufile.write("%10e %10e %10e \n" %(swarm_x[im],swarm_y[im],0.))
+       vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("</Points> \n")
+       #####
+       vtufile.write("<PointData Scalars='scalars'>\n")
+       #--
+       vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Name='velocity' Format='ascii'> \n")
+       for im in range(0,nmarker):
+           vtufile.write("%10e %10e %10e \n" %(swarm_u[im],swarm_v[im],0.))
+       vtufile.write("</DataArray>\n")
+       #--
+       #vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Name='error' Format='ascii'> \n")
+       #for im in range(0,nmarker):
+       #    ui=velocity_x(swarm_x[im],swarm_y[im]) 
+       #    vi=velocity_y(swarm_x[im],swarm_y[im]) 
+       #    vtufile.write("%10e %10e %10e \n" %(swarm_u[im]-ui,swarm_v[im]-vi,0.))
+       #vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("<DataArray type='Float32' NumberOfComponents='1' Name='paint' Format='ascii'> \n")
+       for im in range(0,nmarker):
+           vtufile.write("%10e \n" % swarm_mat[im])
+       vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("</PointData>\n")
+       #####
+       vtufile.write("<Cells>\n")
+       #--
+       vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+       for im in range (0,nmarker):
+           vtufile.write("%d\n" % im )
+       vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+       for im in range (0,nmarker):
+           vtufile.write("%d \n" % (im+1) )
+       vtufile.write("</DataArray>\n")
+       #--
+       vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+       for im in range (0,nmarker):
+           vtufile.write("%d \n" % 1)
        vtufile.write("</DataArray>\n")
        #--
        vtufile.write("</Cells>\n")
