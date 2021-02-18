fc7b6c3ecad91e51cff3d76ceb5ae7af2d7f4840
cedrict <c.thieulot@uu.nl>
2021-02-18 22:37:18 +0100

    f97updt

--- python_codes/fieldstone_97/stone.py
+++ python_codes/fieldstone_97/stone.py
@@ -9,7 +9,7 @@ lonmin=0.5
 lonmax=359.5
 
 #user chosen
-nrad=301  # 301 <-> 300m resolution for -81:9km range
+nrad=13  #301  # 301 <-> 300m resolution for -81:9km range
 
 alpha=3e-5
 rho0=3300
@@ -240,7 +240,7 @@ vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(4*nel,
 #####
 vtufile.write("<Points> \n")
 vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
-counter=0
+#counter=0
 for ilat in range(0,nlat):
     for ilon in range(0,nlon):
         lon=lonmin+ilon*dlon
@@ -271,7 +271,7 @@ for ilat in range(0,nlat):
                                             radius*np.sin(theta)*np.sin(phi),\
                                             radius*np.cos(theta)))
 
-        counter+=1
+        #counter+=1
     #end for
 #end for
 vtufile.write("</DataArray>\n")
@@ -309,3 +309,407 @@ vtufile.write("</VTKFile>\n")
 print('produced moho_shell.vtu')
 
 #########################################################################################
+# counting the number of non-zero volume cells for each layer 
+#########################################################################################
+
+counter =np.zeros(8,dtype=np.int32)
+
+for il in range(0,8): #layer
+
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            boundaries[:]=crust_bnd[:,ilat,ilon]
+            if abs(boundaries[il]-boundaries[il+1])>1e-6:
+               counter[il]+=1
+        #end for
+    #end for
+#end for
+
+print('water      :',counter[0],'cells out of 64800')
+print('ice        :',counter[1],'cells out of 64800')
+print('up. seds.  :',counter[2],'cells out of 64800')
+print('mid. seds. :',counter[3],'cells out of 64800')
+print('low. seds. :',counter[4],'cells out of 64800')
+print('up. crust  :',counter[5],'cells out of 64800')
+print('mid. crust :',counter[6],'cells out of 64800')
+print('low. crust :',counter[7],'cells out of 64800')
+
+#########################################################################################
+# produce 8 layer vtu files
+#########################################################################################
+
+for il in range(0,8): #layer
+
+    filename = 'layer_{:02d}.vtu'.format(il)
+    vtufile=open(filename,"w")
+    vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+    vtufile.write("<UnstructuredGrid> \n")
+    vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(8*counter[il],counter[il]))
+    #####
+    vtufile.write("<Points> \n")
+    vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+            if abs(boundaries[il]-boundaries[il+1])>1e-3:
+               max_depth=boundaries[il]
+               min_depth=boundaries[il+1]
+               r_min=6371e3+min_depth
+               r_max=6371e3+max_depth
+
+               #node 0
+               radius=r_min
+               phi=float(lon-0.5)/180*np.pi
+               theta=np.pi/2-float(lat-0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 1
+               radius=r_min
+               phi=float(lon+0.5)/180*np.pi
+               theta=np.pi/2-float(lat-0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 2
+               radius=r_min
+               phi=float(lon+0.5)/180*np.pi
+               theta=np.pi/2-float(lat+0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 3
+               radius=r_min
+               phi=float(lon-0.5)/180*np.pi
+               theta=np.pi/2-float(lat+0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 4
+               radius=r_max
+               phi=float(lon-0.5)/180*np.pi
+               theta=np.pi/2-float(lat-0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 5
+               radius=r_max
+               phi=float(lon+0.5)/180*np.pi
+               theta=np.pi/2-float(lat-0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 6
+               radius=r_max
+               phi=float(lon+0.5)/180*np.pi
+               theta=np.pi/2-float(lat+0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+               #node 7
+               radius=r_max
+               phi=float(lon-0.5)/180*np.pi
+               theta=np.pi/2-float(lat+0.5)/180*np.pi
+               vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                   radius*np.sin(theta)*np.sin(phi),\
+                                                   radius*np.cos(theta)))
+            #end if
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+    vtufile.write("</Points> \n")
+    #####
+    vtufile.write("<CellData Scalars='scalars'>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='density' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+            if abs(boundaries[il]-boundaries[il+1])>1:
+               vtufile.write("%f\n" % (crust_rho[il,ilat,ilon]*1e3))
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='thickness' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+            if abs(boundaries[il]-boundaries[il+1])>1:
+               vtufile.write("%f\n" % (abs(boundaries[il]-boundaries[il+1])))
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='longitude' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+            if abs(boundaries[il]-boundaries[il+1])>1:
+               vtufile.write("%f\n" % (lon))
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='latitude' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+            if abs(boundaries[il]-boundaries[il+1])>1:
+               vtufile.write("%f\n" % (lat))
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+
+
+
+
+
+    vtufile.write("</CellData>\n")
+    #####
+    vtufile.write("<Cells>\n")
+    vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+    for iel in range (0,counter[il]):
+       vtufile.write("%d %d %d %d %d %d %d %d\n" %(8*iel,8*iel+1,8*iel+2,8*iel+3,
+                                                   8*iel+4,8*iel+5,8*iel+6,8*iel+7))
+    vtufile.write("</DataArray>\n")
+    vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+    for iel in range (0,counter[il]):
+        vtufile.write("%d \n" %((iel+1)*8))
+    vtufile.write("</DataArray>\n")
+    vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+    for iel in range (0,counter[il]):
+        vtufile.write("%d \n" %12)
+    vtufile.write("</DataArray>\n")
+    vtufile.write("</Cells>\n")
+    #####
+    vtufile.write("</Piece>\n")
+    vtufile.write("</UnstructuredGrid>\n")
+    vtufile.write("</VTKFile>\n")
+
+    print('produced '+filename)
+
+#end do
+
+#########################################################################################
+# Same exercise but now with lat-lon bounds
+#########################################################################################
+
+min_lon=60 +180
+max_lon=100 +180 
+
+min_lat=5
+max_lat=40
+
+counter =np.zeros(8,dtype=np.int32)
+
+for il in range(0,8): #layer
+
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]
+               if abs(boundaries[il]-boundaries[il+1])>1e-6:
+                  counter[il]+=1
+        #end for
+    #end for
+#end for
+
+print('min/max lon:',min_lon,max_lon)
+print('min/max lat:',min_lat,max_lat)
+print('water      :',counter[0],'cells out of 64800')
+print('ice        :',counter[1],'cells out of 64800')
+print('up. seds.  :',counter[2],'cells out of 64800')
+print('mid. seds. :',counter[3],'cells out of 64800')
+print('low. seds. :',counter[4],'cells out of 64800')
+print('up. crust  :',counter[5],'cells out of 64800')
+print('mid. crust :',counter[6],'cells out of 64800')
+print('low. crust :',counter[7],'cells out of 64800')
+
+
+
+
+for il in range(0,8): #layer
+
+    filename = 'layer_area_{:02d}.vtu'.format(il)
+    vtufile=open(filename,"w")
+    vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+    vtufile.write("<UnstructuredGrid> \n")
+    vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(8*counter[il],counter[il]))
+    #####
+    vtufile.write("<Points> \n")
+    vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+               if abs(boundaries[il]-boundaries[il+1])>1e-3:
+                  max_depth=boundaries[il]
+                  min_depth=boundaries[il+1]
+                  r_min=6371e3+min_depth
+                  r_max=6371e3+max_depth
+
+                  #node 0
+                  radius=r_min
+                  phi=float(lon-0.5)/180*np.pi
+                  theta=np.pi/2-float(lat-0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 1
+                  radius=r_min
+                  phi=float(lon+0.5)/180*np.pi
+                  theta=np.pi/2-float(lat-0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 2
+                  radius=r_min
+                  phi=float(lon+0.5)/180*np.pi
+                  theta=np.pi/2-float(lat+0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 3
+                  radius=r_min
+                  phi=float(lon-0.5)/180*np.pi
+                  theta=np.pi/2-float(lat+0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 4
+                  radius=r_max
+                  phi=float(lon-0.5)/180*np.pi
+                  theta=np.pi/2-float(lat-0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 5
+                  radius=r_max
+                  phi=float(lon+0.5)/180*np.pi
+                  theta=np.pi/2-float(lat-0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 6
+                  radius=r_max
+                  phi=float(lon+0.5)/180*np.pi
+                  theta=np.pi/2-float(lat+0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+                  #node 7
+                  radius=r_max
+                  phi=float(lon-0.5)/180*np.pi
+                  theta=np.pi/2-float(lat+0.5)/180*np.pi
+                  vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
+                                                      radius*np.sin(theta)*np.sin(phi),\
+                                                      radius*np.cos(theta)))
+               #end if thickness>0
+            #end if lat/lon box
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+    vtufile.write("</Points> \n")
+    #####
+    vtufile.write("<CellData Scalars='scalars'>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='density' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+               if abs(boundaries[il]-boundaries[il+1])>1:
+                  vtufile.write("%f\n" % (crust_rho[il,ilat,ilon]*1e3))
+            #end if lat/lon box
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='thickness' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+               if abs(boundaries[il]-boundaries[il+1])>1:
+                  vtufile.write("%f\n" % (abs(boundaries[il]-boundaries[il+1])))
+            #end if lat/lon box
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+
+    vtufile.write("<DataArray type='Float32' Name='longitude' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+               if abs(boundaries[il]-boundaries[il+1])>1:
+                  vtufile.write("%f\n" % (lon))
+            #end if lat/lon box
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+    vtufile.write("<DataArray type='Float32' Name='latitude' Format='ascii'> \n")
+    for ilat in range(0,nlat):
+        for ilon in range(0,nlon):
+            lon=lonmin+ilon*dlon
+            lat=latmin+ilat*dlat
+            if lon>min_lon and lon<max_lon and lat>min_lat and lat<max_lat: 
+               boundaries[:]=crust_bnd[:,ilat,ilon]*1e3
+               if abs(boundaries[il]-boundaries[il+1])>1:
+                  vtufile.write("%f\n" % (lat))
+            #end if lat/lon box
+        #end for
+    #end for
+    vtufile.write("</DataArray>\n")
+
+
+    vtufile.write("</CellData>\n")
+    #####
+    vtufile.write("<Cells>\n")
+    vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+    for iel in range (0,counter[il]):
+       vtufile.write("%d %d %d %d %d %d %d %d\n" %(8*iel,8*iel+1,8*iel+2,8*iel+3,
+                                                   8*iel+4,8*iel+5,8*iel+6,8*iel+7))
+    vtufile.write("</DataArray>\n")
+    vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+    for iel in range (0,counter[il]):
+        vtufile.write("%d \n" %((iel+1)*8))
+    vtufile.write("</DataArray>\n")
+    vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+    for iel in range (0,counter[il]):
+        vtufile.write("%d \n" %12)
+    vtufile.write("</DataArray>\n")
+    vtufile.write("</Cells>\n")
+    #####
+    vtufile.write("</Piece>\n")
+    vtufile.write("</UnstructuredGrid>\n")
+    vtufile.write("</VTKFile>\n")
+
+    print('produced '+filename)
+
+#end do
+
+#########################################################################################
+
