e3a7848d21fa45c7e1c30a144a24ca050d9e96ee
cedrict <c.thieulot@uu.nl>
2021-03-23 07:57:32 +0100

    f100updt

--- python_codes/fieldstone_100/stone.py
+++ python_codes/fieldstone_100/stone.py
@@ -1,15 +1,48 @@
 import numpy as np
 
 ###############################################################################
+# resolution:
+# 1: 1 degree
+# 2: 0.5 degree
+# 4: 0.25 degree
 
-nlon=360
-nlat=180
+resolution = 16
+
+if resolution==1:
+   nlon=360
+   nlat=180
+   latmin=89.5
+   latmax=-89.5
+   lonmin=0.5
+   lonmax=359.5
+
+if resolution==2:
+   nlon=360*2
+   nlat=180*2
+   latmin=89.75
+   latmax=-89.75
+   lonmin=0.25
+   lonmax=359.75
+
+if resolution==4:
+   nlon=360*4
+   nlat=180*4
+   latmin=89.875
+   latmax=-89.875
+   lonmin=0.125
+   lonmax=359.875
+
+if resolution==16:
+   nlon=360*16
+   nlat=180*16
+   latmin=89.968750
+   latmax=-89.968750
+   lonmin=0.03125
+   lonmax=359.968750
+
 npts=nlon*nlat
 
-latmin=89.5
-latmax=-89.5
-lonmin=0.5
-lonmax=359.5
+print('npts=',npts)
 
 Rmars=3389.5e6
 
@@ -17,9 +50,19 @@ Rmars=3389.5e6
 
 topography =np.zeros((nlat,nlon),dtype=np.float64)
 
-file=open("MOLA_1deg.txt", "r")
+if resolution==1:
+   file=open("MOLA_1deg.txt", "r")
+if resolution==2:
+   file=open("MOLA_0.5deg.txt", "r")
+if resolution==4:
+   file=open("MOLA_0.25deg.txt", "r")
+if resolution==16:
+   file=open("MOLA_0.0625deg.txt", "r")
+
 lines = file.readlines()
 file.close
+
+###############################################################################
 
 counter=0
 for j in range(0,nlat):
@@ -37,6 +80,9 @@ print('moho depth m/M',np.min(topography),np.max(topography))
 dlon=(lonmax-lonmin)/(nlon-1)
 dlat=(latmax-latmin)/(nlat-1)
 
+ddlon=abs(dlon)/2
+ddlat=abs(dlat)/2
+
 nel=nlon*nlat
 
 vtufile=open("topo2D.vtu","w")
@@ -51,10 +97,10 @@ for ilat in range(0,nlat):
     for ilon in range(0,nlon):
             lon=lonmin+ilon*dlon
             lat=latmin+ilat*dlat
-            vtufile.write("%10f %10f %10f \n" %(lon-0.5,lat-0.5,topography[ilat,ilon]/2e3))
-            vtufile.write("%10f %10f %10f \n" %(lon+0.5,lat-0.5,topography[ilat,ilon]/2e3))
-            vtufile.write("%10f %10f %10f \n" %(lon+0.5,lat+0.5,topography[ilat,ilon]/2e3))
-            vtufile.write("%10f %10f %10f \n" %(lon-0.5,lat+0.5,topography[ilat,ilon]/2e3))
+            vtufile.write("%10f %10f %10f \n" %(lon-ddlon,lat-ddlon,topography[ilat,ilon]/2e3))
+            vtufile.write("%10f %10f %10f \n" %(lon+ddlon,lat-ddlon,topography[ilat,ilon]/2e3))
+            vtufile.write("%10f %10f %10f \n" %(lon+ddlon,lat+ddlon,topography[ilat,ilon]/2e3))
+            vtufile.write("%10f %10f %10f \n" %(lon-ddlon,lat+ddlon,topography[ilat,ilon]/2e3))
             counter+=1
     #end for
 #end for
@@ -108,23 +154,23 @@ for ilat in range(0,nlat):
         lat=latmin+ilat*dlat
         radius=Rmars+topography[ilat,ilon]
 
-        phi=float(lon-0.5)/180*np.pi
-        theta=np.pi/2-float(lat-0.5)/180*np.pi
+        phi=float(lon-ddlon)/180*np.pi
+        theta=np.pi/2-float(lat-ddlat)/180*np.pi
         vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
                                             radius*np.sin(theta)*np.sin(phi),\
                                             radius*np.cos(theta)))
-        phi=float(lon+0.5)/180*np.pi
-        theta=np.pi/2-float(lat-0.5)/180*np.pi
+        phi=float(lon+ddlon)/180*np.pi
+        theta=np.pi/2-float(lat-ddlat)/180*np.pi
         vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
                                             radius*np.sin(theta)*np.sin(phi),\
                                             radius*np.cos(theta)))
-        phi=float(lon+0.5)/180*np.pi
-        theta=np.pi/2-float(lat+0.5)/180*np.pi
+        phi=float(lon+ddlon)/180*np.pi
+        theta=np.pi/2-float(lat+ddlat)/180*np.pi
         vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
                                             radius*np.sin(theta)*np.sin(phi),\
                                             radius*np.cos(theta)))
-        phi=float(lon-0.5)/180*np.pi
-        theta=np.pi/2-float(lat+0.5)/180*np.pi
+        phi=float(lon-ddlon)/180*np.pi
+        theta=np.pi/2-float(lat+ddlat)/180*np.pi
         vtufile.write("%10f %10f %10f \n" %(radius*np.sin(theta)*np.cos(phi),\
                                             radius*np.sin(theta)*np.sin(phi),\
                                             radius*np.cos(theta)))
