facdd45a7104ff929e34a445aef00a050e5f41c0
cedrict <c.thieulot@uu.nl>
2022-10-12 18:05:08 +0200

    f134updt

--- python_codes/fieldstone_134/stone.py
+++ python_codes/fieldstone_134/stone.py
@@ -1,6 +1,10 @@
 import numpy as np
 import time 
 
+print("-----------------------------")
+print("--------- stone 134 ---------")
+print("-----------------------------")
+
 ###############################################################################
 start = time.time()
 
@@ -53,14 +57,14 @@ for il in range(0,C_nb):
     #print(vals)
     #print(il*5+3,len(vals))
     #print(vals[1])
-    if abs(float(vals[1]))<1e-7: 
-       print(il,vals[1])
-    C_Mrr[il]=float(vals[1])#*10**float(vals[0])
-    C_Mtt[il]=float(vals[3])#*10**float(vals[0])
-    C_Mpp[il]=float(vals[5])#*10**float(vals[0])
-    C_Mrt[il]=float(vals[7])#*10**float(vals[0])
-    C_Mrp[il]=float(vals[9])#*10**float(vals[0])
-    C_Mtp[il]=float(vals[11])#*10**float(vals[0])
+    #if abs(float(vals[1]))<1e-7: 
+    #   print(il,vals[1])
+    C_Mrr[il]=float(vals[1]) *10**float(vals[0])
+    C_Mtt[il]=float(vals[3]) *10**float(vals[0])
+    C_Mpp[il]=float(vals[5]) *10**float(vals[0])
+    C_Mrt[il]=float(vals[7]) *10**float(vals[0])
+    C_Mrp[il]=float(vals[9]) *10**float(vals[0])
+    C_Mtp[il]=float(vals[11])*10**float(vals[0])
 
 C_M=np.sqrt(0.5*(C_Mrr**2+C_Mtt**2+C_Mpp**2)+C_Mrt**2+C_Mrp**2+C_Mtp**2)
 
@@ -93,7 +97,7 @@ C_z=C_r*np.cos(C_theta)
 #------------------------------------------------------------------------------
    
 if True:
-   vtufile=open("location.vtu","w")
+   vtufile=open("cmt.vtu","w")
    vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
    vtufile.write("<UnstructuredGrid> \n")
    vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(C_nb,C_nb))
@@ -168,4 +172,137 @@ if True:
    vtufile.write("</UnstructuredGrid>\n")
    vtufile.write("</VTKFile>\n")
    vtufile.close()
+
+###############################################################################
+# I have removed the first 3 columns off the .csv file downloaded 
+# from the cite (in open office), and copy-pasted the result in a txt file
+###############################################################################
+
+file=open("EuroMedCentrMomTensors.txt", "r")
+lines = file.readlines()
+file.close
+
+C_nb=int(2905)
+
+C_lon=np.zeros(C_nb,dtype=np.float64)
+C_lat=np.zeros(C_nb,dtype=np.float64)
+C_time=np.zeros(C_nb,dtype=np.float64)
+C_depth=np.zeros(C_nb,dtype=np.float64)
+C_Mrr=np.zeros(C_nb,dtype=np.float64)
+C_Mtt=np.zeros(C_nb,dtype=np.float64)
+C_Mpp=np.zeros(C_nb,dtype=np.float64)
+C_Mrt=np.zeros(C_nb,dtype=np.float64)
+C_Mrp=np.zeros(C_nb,dtype=np.float64)
+C_Mb=np.zeros(C_nb,dtype=np.float64)
+
+for il in range(0,C_nb):
+    vals=lines[il].strip().split()
+    C_lat[il]=float(vals[0])
+    C_lon[il]=float(vals[1])
+    C_depth[il]=float(vals[2])
+    C_Mb[il]=float(vals[3])
+
+print('----------------------------------------')
+print('latitude (m/M)',min(C_lat),max(C_lat))
+print('longitude (m/M)',min(C_lon),max(C_lon))
+print('depth (m/M)',min(C_depth),max(C_depth))
+print('Mrr (m/M)',min(C_Mb),max(C_Mb))
+#print('Mrr (m/M)',min(C_Mrr),max(C_Mrr))
+#print('Mtt (m/M)',min(C_Mtt),max(C_Mtt))
+#print('Mpp (m/M)',min(C_Mpp),max(C_Mpp))
+#print('Mrt (m/M)',min(C_Mrt),max(C_Mrt))
+#print('Mrp (m/M)',min(C_Mrp),max(C_Mrp))
+#print('Mtp (m/M)',min(C_Mtp),max(C_Mtp))
+print('----------------------------------------')
+
+#------------------------------------------------------------------------------
+
+C_phi=C_lon/180*np.pi
+C_theta=(90-C_lat)/180*np.pi
+C_r=6371e3-C_depth*1e3
+
+C_x=C_r*np.sin(C_theta)*np.cos(C_phi)
+C_y=C_r*np.sin(C_theta)*np.sin(C_phi)
+C_z=C_r*np.cos(C_theta)
+
+if True:
+   vtufile=open("cmt_mediterranean.vtu","w")
+   vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+   vtufile.write("<UnstructuredGrid> \n")
+   vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(C_nb,C_nb))
+   #####
+   vtufile.write("<Points> \n")
+   vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10f %10f %10f \n" %(C_x[i],C_y[i],C_z[i]))
+   vtufile.write("</DataArray>\n")
+   vtufile.write("</Points> \n")
+   #####
+   vtufile.write("<PointData>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mrr' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mrr[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mtt' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mtt[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mpp' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mpp[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mrt' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mrt[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mrp' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mrp[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mtp' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mtp[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='M (effective)' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_M[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+   vtufile.write("<DataArray type='Float32' Name='Mb' Format='ascii'> \n")
+   for i in range(0,C_nb):
+       vtufile.write("%10e \n" %(C_Mb[i]))
+   vtufile.write("</DataArray>\n")
+   #--
+
+
+   vtufile.write("</PointData>\n")
+   #####
+   vtufile.write("<Cells>\n")
+   vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+   for i in range (0,C_nb):
+       vtufile.write("%d \n" % i)
+   vtufile.write("</DataArray>\n")
+   vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+   for i in range (0,C_nb):
+       vtufile.write("%d \n" %(i+1))
+   vtufile.write("</DataArray>\n")
+   vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+   for iel in range (0,C_nb):
+       vtufile.write("%d \n" % 1)
+   vtufile.write("</DataArray>\n")
+   vtufile.write("</Cells>\n")
+   #####
+   vtufile.write("</Piece>\n")
+   vtufile.write("</UnstructuredGrid>\n")
+   vtufile.write("</VTKFile>\n")
+   vtufile.close()
+
+
 
