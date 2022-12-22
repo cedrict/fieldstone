16309707f8f30c15850dde0d0261eee6767ac3dc
cedrict <c.thieulot@uu.nl>
2022-12-22 12:13:46 +0100

    f138updt

--- python_codes/fieldstone_138/stone.py
+++ python_codes/fieldstone_138/stone.py
@@ -5,7 +5,36 @@ import random
 import time as time
 
 #------------------------------------------------------
-def compute_benchmark_sphere(x,y,z,R,Mz,xcenter,ycenter,zcenter,benchmark):
+
+def topography(x,y,A,llambda,dx,dy,slopex,slopey):
+    pert1=A*np.sin(2*np.pi/llambda*(x*dx+y*dy))
+    pert2=slopex*x+slopey*y 
+    return pert1+pert2
+
+#------------------------------------------------------
+# returns analytical solution (vector B) 
+
+#TODO: implement other solutions?
+
+def compute_analytical_solution(x,y,z,R,Mz,xcenter,ycenter,zcenter,benchmark):
+
+   if benchmark==1:
+
+      mu0=4*np.pi #*1e-7
+
+      V=4/3*np.pi*Rsphere**3
+
+      r=np.sqrt((x-xcenter)**2+(y-ycenter)**2+(z-zcenter)**2)
+
+      Bx=0
+      By=0
+      Bz=2*mu0*V/4/np.pi/r**3*Mz0
+
+   if benchmark==2 or benchmark==4:
+
+      Bx=0
+      By=0
+      Bz=0
 
    if benchmark==3:
 
@@ -30,7 +59,7 @@ def compute_benchmark_sphere(x,y,z,R,Mz,xcenter,ycenter,zcenter,benchmark):
       By=Q*Mz*(2*(ruy*np.cos(theta))+thuy*np.sin(theta))
       Bz=Q*Mz*(2*(ruz*np.cos(theta))+thuz*np.sin(theta))
 
-      return np.array([Bx,By,Bz],dtype=np.float64)
+   return np.array([Bx,By,Bz],dtype=np.float64)
 
 #------------------------------------------------------
 
@@ -56,36 +85,43 @@ nny_meas=17
 
 #benchmark:
 #1: dipole (small sphere, far away)
-#2: random perturbation internal nodes
-#3: sphere (larger sphere, anywhere in space)
+#2: random perturbation internal nodes -> checks cancellation of internal faces 
+#3: sphere (larger sphere, anywhere in space) analytical
+#4: wavy surface
 
-benchmark=3
+benchmark=4
 
 #################################################################
+# be careful with the position of the measurement points for 
+# benchmark 1. These cannot be above the center of the sphere
+# but also not above a diagonal of an element (see ...wtopo)
+
 if benchmark==1:
    Lx=2
    Ly=2
    Lz=2
-   nelx=10
-   nely=10
-   nelz=10
+   nelx=40
+   nely=40
+   nelz=40
    Rsphere=1
-   Mx0=0
-   My0=0
-   Mz0=1
+   Mx0=0     # do not change
+   My0=0     # do not change
+   Mz0=1     # do not change
    nqdim=4
    xcenter=Lx/2
    ycenter=Ly/2
    zcenter=-Lz/2
    #line meas
-   xstart=Lx/2
-   ystart=Ly/2
-   zstart=0.01
-   xend=Lx/2
-   yend=Ly/2
-   zend=10*Rsphere
+   do_line_measurements=True
+   xstart=Lx/2+1e-9
+   ystart=Ly/2+1e-10
+   zstart=0.01      #slightly above surface
+   xend=Lx/2+1e-9
+   yend=Ly/2+1e-10
+   zend=10
    nmeas=100 
    #plane meas
+   do_plane_measurements=False
    x0=-Lx/2
    y0=-Ly/2
    z0=zend
@@ -106,6 +142,7 @@ if benchmark==2:
    Mz0=0
    nqdim=6
    #plane meas
+   do_plane_measurements=True
    x0=-Lx/2
    y0=-Ly/2
    z0=1
@@ -114,6 +151,11 @@ if benchmark==2:
    nnx_meas=11
    nny_meas=11
    dz=0.1 #amplitude random
+   do_line_measurements=False
+   Rsphere=0
+   xcenter=0
+   ycenter=0
+   zcenter=0
 
 if benchmark==3:
    Lx=20
@@ -131,6 +173,7 @@ if benchmark==3:
    zcenter=-Lz/2
    nqdim=4
    #plane meas
+   do_plane_measurements=True
    x0=-Lx/2
    y0=-Ly/2
    z0=0.4
@@ -138,6 +181,47 @@ if benchmark==3:
    Ly_meas=2*Ly
    nnx_meas=30
    nny_meas=30
+   do_line_measurements=False
+
+if benchmark==4:
+   Lx=100
+   Ly=100
+   Lz=100
+   nelx=50
+   nely=50
+   nelz=12
+   Mx0=0
+   My0=4
+   Mz0=-6
+   wavelength=25
+   A=1
+   angle=20/180*np.pi
+   dx=np.cos(angle)
+   dy=np.sin(angle)
+   do_plane_measurements=True
+   x0=-Lx/2
+   y0=-Ly/2
+   z0=0.25
+   Lx_meas=2*Lx
+   Ly_meas=2*Ly
+   nnx_meas=30
+   nny_meas=30
+   do_line_measurements=False
+   Rsphere=0
+   xcenter=0
+   ycenter=0
+   zcenter=0
+   slopex=np.arctan(3/180*np.pi)
+   slopey=np.arctan(5/180*np.pi)
+   #TODO stretch all nodes vertical
+   #TODO: code line meas 
+
+   #subbench=1,2,3,4
+   #if subbench==1:
+   #if subbench==2:
+   #if subbench==3:
+   #if subbench==4:
+
 
 #-----------------------------------------------
 
@@ -149,6 +233,8 @@ NV=nnx*nny*nnz  # number of nodes
 
 #################################################################
 # grid point setup
+# if benchmark 2, a small random perturbation is added to the
+# z coordinate of the interior nodes.
 #################################################################
 
 x = np.empty(NV,dtype=np.float64)  # x coordinates
@@ -193,7 +279,20 @@ for i in range(0,nelx):
 #end for
 
 #################################################################
+# adding wavy topography to surface
+
+if benchmark==4:
+
+   for i in range(0,NV):
+       if abs(z[i])<1e-6:
+          z[i]+=topography(x[i]-Lx/2,y[i]-Ly/2,A,wavelength,dx,dy,slopex,slopey)
+
+
+#################################################################
 # prescribe M inside each cell
+# for benchmarks 1 and 3, M is zero everywhere except inside
+# a sphere of radius Rsphere at location (xcenter,ycenter,zcenter)
+# For benchmark 2, M is constant in space and equal to (Mx0,My0,Mz0)
 
 Mx=np.zeros(nel,dtype=np.float64)
 My=np.zeros(nel,dtype=np.float64)
@@ -224,100 +323,135 @@ if benchmark==2:
 export_mesh_3D(NV,nel,x,y,z,icon,'mesh.vtu',Mx,My,Mz)
 
 #################################################################
+# plane measurements setup
+# the plane originates at (x0,y0,z0) and extends in the x,y
+# directions by Lx_meas,Ly_meas
+# note that a small perturbation is added to the x,y coordinates
+# so as to avoid that a measurement point lies in the plane
+# of an element (vertical) face. 
+#################################################################
 
-N_meas=nnx_meas*nny_meas
+if do_plane_measurements:
 
-nelx_meas=nnx_meas-1
-nely_meas=nny_meas-1
-nel_meas=nelx_meas*nely_meas
+   N_meas=nnx_meas*nny_meas
 
-x_meas = np.empty(N_meas,dtype=np.float64)  # x coordinates
-y_meas = np.empty(N_meas,dtype=np.float64)  # y coordinates
-z_meas = np.empty(N_meas,dtype=np.float64)  # y coordinates
+   nelx_meas=nnx_meas-1
+   nely_meas=nny_meas-1
+   nel_meas=nelx_meas*nely_meas
 
-counter = 0
-for j in range(0,nny_meas):
-    for i in range(0,nnx_meas):
-        x_meas[counter]=x0+(i+random.uniform(-1,+1)*1e-8)*Lx_meas/float(nnx_meas-1) 
-        y_meas[counter]=y0+(j+random.uniform(-1,+1)*1e-8)*Ly_meas/float(nny_meas-1) 
-        z_meas[counter]=z0
-        counter += 1
+   x_meas = np.empty(N_meas,dtype=np.float64)  # x coordinates
+   y_meas = np.empty(N_meas,dtype=np.float64)  # y coordinates
+   z_meas = np.empty(N_meas,dtype=np.float64)  # y coordinates
 
-icon_meas =np.zeros((4,nel_meas),dtype=np.int32)
-counter = 0
-for j in range(0,nely_meas):
-    for i in range(0,nelx_meas):
-        icon_meas[0, counter] = i + j * (nelx_meas + 1)
-        icon_meas[1, counter] = i + 1 + j * (nelx_meas + 1)
-        icon_meas[2, counter] = i + 1 + (j + 1) * (nelx_meas + 1)
-        icon_meas[3, counter] = i + (j + 1) * (nelx_meas + 1)
-        counter += 1
+   counter = 0
+   for j in range(0,nny_meas):
+       for i in range(0,nnx_meas):
+           x_meas[counter]=x0+(i+random.uniform(-1,+1)*1e-8)*Lx_meas/float(nnx_meas-1) 
+           y_meas[counter]=y0+(j+random.uniform(-1,+1)*1e-8)*Ly_meas/float(nny_meas-1) 
+           z_meas[counter]=z0
+           if benchmark==4:
+              z_meas[counter]+=topography(x_meas[counter]-Lx/2,y_meas[counter]-Ly/2,A,wavelength,dx,dy,slopex,slopey)
+           counter += 1
 
-export_mesh_2D(N_meas,nel_meas,x_meas,y_meas,z_meas,icon_meas,'mesh_meas.vtu')
+   icon_meas =np.zeros((4,nel_meas),dtype=np.int32)
+   counter = 0
+   for j in range(0,nely_meas):
+       for i in range(0,nelx_meas):
+           icon_meas[0, counter] = i + j * (nelx_meas + 1)
+           icon_meas[1, counter] = i + 1 + j * (nelx_meas + 1)
+           icon_meas[2, counter] = i + 1 + (j + 1) * (nelx_meas + 1)
+           icon_meas[3, counter] = i + (j + 1) * (nelx_meas + 1)
+           counter += 1
 
-print('setup plane measurement points ')
+   export_mesh_2D(N_meas,nel_meas,x_meas,y_meas,z_meas,icon_meas,'mesh_meas.vtu')
+
+   print('setup plane measurement points ')
 
 #################################################################
 # measuring B on a plane
+# Nomenclature:
+# _vi: volume integral
+# _si: surface integral
+# _th: analytical value (if applicable)
+# The volume integral is parameterised by the number of quadrature 
+# points per dimension nqdim.
+# Because the integrand is not a polynomial, the volume integral
+# remains a numerical solution, while the surface integral 
+# is actually analytical (down to machine precision).
 #################################################################
+   
+print('*****************************')
+print('***** plane measurement *****')
+print('*****************************')
 
-
-
-B_vi=np.zeros((3,N_meas),dtype=np.float64)
-B_si=np.zeros((3,N_meas),dtype=np.float64)
-B_th=np.zeros((3,N_meas),dtype=np.float64)
-
-for i in range(0,N_meas):
-    print('------------------------------')
-    print('doing',i,'out of ',N_meas) 
-    print('x,y,z meas',x_meas[i],y_meas[i],z_meas[i])
-    B_th[:,i]=compute_benchmark_sphere(x_meas[i],y_meas[i],z_meas[i],Rsphere,Mz0,xcenter,ycenter,zcenter,benchmark)
-    print('analytical ->',B_th[:,i])
-
-    start = time.time()
-    for iel in range(0,nel):
-        B_vi[:,i]+=compute_B_quadrature(x_meas[i],y_meas[i],z_meas[i],x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel],nqdim)
-    print("vol int: %.3f s" % (time.time() - start))
-    print('vol int    ->',B_vi[:,i])
-
-    for iel in range(0,nel):
-        B_si[:,i]+=compute_B_surface_integral_wtopo(x_meas[i],y_meas[i],z_meas[i],x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel])
-    print("surf int: %.3f s" % (time.time() - start))
-    print('surf int   ->',B_si[:,i])
-
-export_measurements(N_meas,nel_meas,x_meas,y_meas,z_meas,icon_meas,'plane_measurements.vtu',B_vi,B_si,B_th)
-
-np.savetxt('plane_measurements.ascii',np.array([x_meas,y_meas,z_meas,\
-                                                B_vi[0,:],B_vi[1,:],B_vi[2,:],\
-                                                B_th[0,:],B_th[1,:],B_th[2,:]]).T)
-
-
-#################################################################
-# measuring B on a line
-#################################################################
-
-if benchmark==1:
-
-   print('*********************')
-   print('***** line meas *****')
-   print('*********************')
-
-   linefile=open("measurements_line.ascii","w")
+if do_plane_measurements:
 
    B_vi=np.zeros((3,N_meas),dtype=np.float64)
    B_si=np.zeros((3,N_meas),dtype=np.float64)
+   B_th=np.zeros((3,N_meas),dtype=np.float64)
+
+   for i in range(0,N_meas):
+       print('------------------------------')
+       print('doing',i,'out of ',N_meas) 
+       print('x,y,z meas',x_meas[i],y_meas[i],z_meas[i])
+       B_th[:,i]=compute_analytical_solution(x_meas[i],y_meas[i],z_meas[i],Rsphere,Mz0,xcenter,ycenter,zcenter,benchmark)
+       print('analytical ->',B_th[:,i])
+
+       start = time.time()
+       for iel in range(0,nel):
+           B_vi[:,i]+=compute_B_quadrature(x_meas[i],y_meas[i],z_meas[i],x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel],nqdim)
+       print("vol int: %.3f s" % (time.time() - start))
+       print('vol int    ->',B_vi[:,i])
+
+       start = time.time()
+       for iel in range(0,nel):
+           B_si[:,i]+=compute_B_surface_integral_wtopo(x_meas[i],y_meas[i],z_meas[i],x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel])
+       print("surf int: %.3f s" % (time.time() - start))
+       print('surf int   ->',B_si[:,i])
+
+   export_measurements(N_meas,nel_meas,x_meas,y_meas,z_meas,icon_meas,'plane_measurements.vtu',B_vi,B_si,B_th)
+
+   np.savetxt('plane_measurements.ascii',np.array([x_meas,y_meas,z_meas,\
+                                                   B_vi[0,:],B_vi[1,:],B_vi[2,:],\
+                                                   B_th[0,:],B_th[1,:],B_th[2,:]]).T)
+
+#################################################################
+# measuring B on a line
+# the line starts at xstart,ystart,zstart and ends at 
+# xend,yend,zend, and is discretised by means of nmeas
+#################################################################
+#TODO: rename nmeas
+
+if do_line_measurements:
+
+   print('****************************')
+   print('***** line measurement *****')
+   print('****************************')
+
+   linefile=open("measurements_line.ascii","w")
+
+   B_vi=np.zeros((3,nmeas),dtype=np.float64)
+   B_si=np.zeros((3,nmeas),dtype=np.float64)
+   B_th=np.zeros((3,nmeas),dtype=np.float64)
 
    for i in range(0,nmeas):
        print('doing',i,'out of ',nmeas) 
        xm=xstart+(xend-xstart)/(nmeas-1)*i
        ym=ystart+(yend-ystart)/(nmeas-1)*i
        zm=zstart+(zend-zstart)/(nmeas-1)*i
+       print(xm,ym,zm)
        for iel in range(0,nel):
            B_vi[:,i]+=compute_B_quadrature      (xm,ym,zm,x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel],nqdim)
            B_si[:,i]+=compute_B_surface_integral_wtopo(xm,ym,zm,x,y,z,icon[:,iel],Mx[iel],My[iel],Mz[iel])
+
+       B_th[:,i]=compute_analytical_solution(xm,ym,zm,Rsphere,Mz0,xcenter,ycenter,zcenter,benchmark)
+       #print(B_th[:,i]) 
+       #print(B_vi[:,i]) 
+       #print(B_si[:,i]) 
     
-       linefile.write("%e %e %e %e %e %e %e %e %e\n" %(xm,ym,zm,\
+       linefile.write("%e %e %e %e %e %e %e %e %e %e %e %e \n" %(xm,ym,zm,\
                                                        B_vi[0,i],B_vi[1,i],B_vi[2,i],\
-                                                       B_si[0,i],B_si[1,i],B_si[2,i]))
+                                                       B_si[0,i],B_si[1,i],B_si[2,i],\
+                                                       B_th[0,i],B_th[1,i],B_th[2,i]))
 
 
