c4c2c96a612be59c81e87868f13ad61c9747f1d7
cedric thieulot <c.thieulot@uu.nl>
2024-06-10 10:55:58 +0200

    f159updt

--- python_codes/fieldstone_159/stone.py
+++ python_codes/fieldstone_159/stone.py
@@ -1,18 +1,16 @@
 import numpy as np
 import time
 
-make_vtu=True
 
 c=20e6
 phi=20./180.*np.pi
-eps=1e5
 
-c_GM=c/20
+plane_stress=2e7
 
 Lx=1
 Ly=1
 Lz=1
-nnx=80#0
+nnx=199
 nny=nnx
 nnz=nnx
 nelx=nnx-1
@@ -21,6 +19,18 @@ nelz=nnz-1
 m=8
 NV=nnx*nny*nnz
 nel=(nnx-1)*(nny-1)*(nnz-1)
+
+if nnx<301:
+   eps=4e5
+elif nnx<401:
+   eps=2e5
+else:
+   eps=1e5
+
+if nnx>100:
+   make_vtu=False
+else:
+   make_vtu=True
 
 ###############################################################################
 # grid point setup
@@ -86,13 +96,13 @@ DPcfile=open('DPc.ascii',"w")
 DPmfile=open('DPm.ascii',"w")
 GMfile=open('GM.ascii',"w")
 
-VMfile_xy=open('VM_xy.ascii',"w")
-TRfile_xy=open('TR_xy.ascii',"w")
-MCfile_xy=open('MC_xy.ascii',"w")
-DPifile_xy=open('DPi_xy.ascii',"w")
-DPcfile_xy=open('DPc_xy.ascii',"w")
-DPmfile_xy=open('DPm_xy.ascii',"w")
-GMfile_xy=open('GM_xy.ascii',"w")
+VMfile_xyz=open('VM_xyz.ascii',"w")
+TRfile_xyz=open('TR_xyz.ascii',"w")
+MCfile_xyz=open('MC_xyz.ascii',"w")
+DPifile_xyz=open('DPi_xyz.ascii',"w")
+DPcfile_xyz=open('DPc_xyz.ascii',"w")
+DPmfile_xyz=open('DPm_xyz.ascii',"w")
+GMfile_xyz=open('GM_xyz.ascii',"w")
 
 VMfile_plane=open('VM_plane.ascii',"w")
 TRfile_plane=open('TR_plane.ascii',"w")
@@ -104,6 +114,12 @@ GMfile_plane=open('GM_plane.ascii',"w")
 
 ang0file=open('pi0_plane.ascii',"w")
 ang6file=open('pi6_plane.ascii',"w")
+
+xfile=open('sigma1.ascii',"w")
+yfile=open('sigma2.ascii',"w")
+zfile=open('sigma3.ascii',"w")
+diagfile=open('xyz.ascii',"w")
+pfile=open('pressure.ascii',"w")
 
 sqrt3=np.sqrt(3)
 onedeg=1./180.*np.pi
@@ -122,6 +138,7 @@ if make_vtu:
    array_DPi= np.empty(NV,dtype=np.float64)  
    array_DPm= np.empty(NV,dtype=np.float64)  
    array_GM= np.empty(NV,dtype=np.float64)  
+   plane=np.zeros(NV,dtype=np.float64)  
 
 counter=0
 for x in xvals:
@@ -155,73 +172,95 @@ for x in xvals:
             sintheta=np.sin(theta)
             r=np.sqrt(x**2+y**2)
 
-            if abs(x+y+z-1e7)<eps:
+            if make_vtu: plane[counter]=x+y+z-plane_stress
+            if abs(x+y+z-plane_stress)<eps:
                if abs(theta)<onedeg:
                   ang0file.write("%10e %10e %10e \n" %(x,y,z))
                if abs(theta-np.pi/6.)<onedeg:
                   ang6file.write("%10e %10e %10e \n" %(x,y,z))
 
+            if x>0 and abs(y)<eps and abs(z)<eps:
+               xfile.write("%10e %10e %10e \n" %(x,y,z))
+            if y>0 and abs(x)<eps and abs(z)<eps:
+               yfile.write("%10e %10e %10e \n" %(x,y,z))
+            if z>0 and abs(x)<eps and abs(y)<eps:
+               zfile.write("%10e %10e %10e \n" %(x,y,z))
+            if abs(x-y)<eps and abs(x-z)<eps and abs(y-z)<eps and x>=0 and y>=0 and z>=0:
+               diagfile.write("%10e %10e %10e \n" %(x,y,z))
+
+            p=-I1sig/3
+            if abs(x-y)<eps and abs(x-z)<eps and abs(y-z)<eps and p>=0:
+               pfile.write("%10e %10e %10e \n" %(x,y,z))
+               
+
             #von Mises
-            if make_vtu: array_vM[counter]=sqrtI2tau-c
-            if abs(sqrtI2tau-c)<eps:
+            FFF=sqrt3*sqrtI2tau-c
+            if make_vtu: array_vM[counter]=FFF
+            if abs(FFF)<eps:
                VMfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  VMfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  VMfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   VMfile_plane.write("%10e %10e %10e \n" %(x,y,z))
 
             #Tresca
-            if make_vtu: array_TR[counter]=abs(2*sqrtI2tau*costheta-c)
-            if abs(2*sqrtI2tau*costheta-c)<eps:
+            FFF=2*sqrtI2tau*costheta-c
+            if make_vtu: array_TR[counter]=FFF
+            if abs(FFF)<eps:
                TRfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  TRfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  TRfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   TRfile_plane.write("%10e %10e %10e \n" %(x,y,z))
                 
             #Mohr-Coulomb
-            if make_vtu: array_MC[counter]=sqrtI2tau*(costheta-sintheta*sinphi/sqrt3)+I1sig/3.*sinphi-c*cosphi
-            if abs(sqrtI2tau*(costheta-sintheta*sinphi/sqrt3)+I1sig/3.*sinphi-c*cosphi)<eps:
+            FFF=sqrtI2tau*(costheta-sintheta*sinphi/sqrt3)+I1sig/3.*sinphi-c*cosphi
+            if make_vtu: array_MC[counter]=FFF
+            if abs(FFF)<eps:
                MCfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  MCfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  MCfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   MCfile_plane.write("%10e %10e %10e \n" %(x,y,z))
 
             #Drucker-Prager circumscribe 
-            if make_vtu: array_DPc[counter]=sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3-sinphi))
-            if abs(sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3-sinphi)))<eps:
+            FFF=sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3-sinphi))
+            if make_vtu: array_DPc[counter]=FFF
+            if abs(FFF)<eps:
                DPcfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  DPcfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  DPcfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   DPcfile_plane.write("%10e %10e %10e \n" %(x,y,z))
 
             #Drucker-Prager middle-circumscribe
-            if make_vtu: array_DPm[counter]=sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3+sinphi))
-            if abs(sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3+sinphi)))<eps:
+            FFF=sqrtI2tau-(-6*sinphi*I1sig/3 + 6*c*cosphi)/(sqrt3*(3+sinphi))
+            if make_vtu: array_DPm[counter]=FFF
+            if abs(FFF)<eps:
                DPmfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  DPmfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  DPmfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   DPmfile_plane.write("%10e %10e %10e \n" %(x,y,z))
 
             #Drucker-Prager inscribe
-            if make_vtu: array_DPi[counter]=sqrtI2tau-(-3*sinphi*I1sig/3 + 3*c*cosphi)/np.sqrt(9+3*sinphi**2)
-            if abs(sqrtI2tau-(-3*sinphi*I1sig/3 + 3*c*cosphi)/np.sqrt(9+3*sinphi**2))<eps:
+            FFF=sqrtI2tau-(-3*sinphi*I1sig/3 + 3*c*cosphi)/np.sqrt(9+3*sinphi**2)
+            if make_vtu: array_DPi[counter]=FFF
+            if abs(FFF)<eps:
                DPifile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  DPifile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  DPifile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   DPifile_plane.write("%10e %10e %10e \n" %(x,y,z))
 
             # Griffith-Murrell
-            if make_vtu: array_GM[counter]= (x-y)**2+(y-z)**2+(z-x)**2+24*c_GM*(x+y+z)
-            if abs( (x-y)**2+(y-z)**2+(z-x)**2+24*c*(x+y+z) )<eps:
+            FFF= (x-y)**2+(y-z)**2+(z-x)**2+24*c*(x+y+z)
+            if make_vtu: array_GM[counter]=FFF
+            if abs(FFF)<eps:
                GMfile.write("%10e %10e %10e \n" %(x,y,z))
                if abs(x-y)<eps:
-                  GMfile_xy.write("%10e %10e %10e \n" %(x,y,z))
-               if abs(x+y+z-1e7)<eps:
+                  GMfile_xyz.write("%10e %10e %10e \n" %(x,y,z))
+               if abs(x+y+z-plane_stress)<eps:
                   GMfile_plane.write("%10e %10e %10e \n" %(x,y,z))
          
             counter+=1
@@ -294,6 +333,11 @@ if make_vtu:
    for i in range (0,NV):
        vtufile.write("%e\n" % array_GM[i])
    vtufile.write("</DataArray>\n")
+   vtufile.write("<DataArray type='Float32' Name='plane' Format='ascii'> \n")
+   for i in range (0,NV):
+       vtufile.write("%e\n" % plane[i])
+   vtufile.write("</DataArray>\n")
+
 
    vtufile.write("</PointData>\n")
    #####
