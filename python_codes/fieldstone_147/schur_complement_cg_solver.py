16102dab49bc38f258ba9906b06ba36ebe0f67a1
cedric thieulot <c.thieulot@uu.nl>
2025-01-27 09:13:59 +0100

    f147updt

--- python_codes/fieldstone_147/schur_complement_cg_solver.py
+++ python_codes/fieldstone_147/schur_complement_cg_solver.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy.sparse as sps
-from numba import jit
 import scipy.sparse.linalg as sla
 
 ############################################################################### 
