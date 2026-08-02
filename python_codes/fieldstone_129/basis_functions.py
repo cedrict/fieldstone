07c43d2c17c95b4fd13a7a00387910120b2e4a8c
cedric thieulot <c.thieulot@uu.nl>
2026-08-02 21:43:04 +0200

    f129clean

---
+++ python_codes/fieldstone_129/basis_functions.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def basis_functions_V(r,s):
+    N0= 0.5*r*(r-1.) * 0.5*s*(s-1.)
+    N1= 0.5*r*(r+1.) * 0.5*s*(s-1.)
+    N2= 0.5*r*(r+1.) * 0.5*s*(s+1.)
+    N3= 0.5*r*(r-1.) * 0.5*s*(s+1.)
+    N4=    (1.-r**2) * 0.5*s*(s-1.)
+    N5= 0.5*r*(r+1.) *    (1.-s**2)
+    N6=    (1.-r**2) * 0.5*s*(s+1.)
+    N7= 0.5*r*(r-1.) *    (1.-s**2)
+    N8=    (1.-r**2) *    (1.-s**2)
+    return np.array([N0,N1,N2,N3,N4,N5,N6,N7,N8],dtype=np.float64)
+
+def basis_functions_V_dr(r,s):
+    dNdr0= 0.5*(2.*r-1.) * 0.5*s*(s-1)
+    dNdr1= 0.5*(2.*r+1.) * 0.5*s*(s-1)
+    dNdr2= 0.5*(2.*r+1.) * 0.5*s*(s+1)
+    dNdr3= 0.5*(2.*r-1.) * 0.5*s*(s+1)
+    dNdr4=       (-2.*r) * 0.5*s*(s-1)
+    dNdr5= 0.5*(2.*r+1.) *   (1.-s**2)
+    dNdr6=       (-2.*r) * 0.5*s*(s+1)
+    dNdr7= 0.5*(2.*r-1.) *   (1.-s**2)
+    dNdr8=       (-2.*r) *   (1.-s**2)
+    return np.array([dNdr0,dNdr1,dNdr2,dNdr3,dNdr4,dNdr5,dNdr6,dNdr7,dNdr8],dtype=np.float64)
+
+def basis_functions_V_ds(r,s):
+    dNds0= 0.5*r*(r-1.) * 0.5*(2.*s-1.)
+    dNds1= 0.5*r*(r+1.) * 0.5*(2.*s-1.)
+    dNds2= 0.5*r*(r+1.) * 0.5*(2.*s+1.)
+    dNds3= 0.5*r*(r-1.) * 0.5*(2.*s+1.)
+    dNds4=    (1.-r**2) * 0.5*(2.*s-1.)
+    dNds5= 0.5*r*(r+1.) *       (-2.*s)
+    dNds6=    (1.-r**2) * 0.5*(2.*s+1.)
+    dNds7= 0.5*r*(r-1.) *       (-2.*s)
+    dNds8=    (1.-r**2) *       (-2.*s)
+    return np.array([dNds0,dNds1,dNds2,dNds3,dNds4,dNds5,dNds6,dNds7,dNds8],dtype=np.float64)
