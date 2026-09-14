2f1c17d90941d8b26bfa69127f9b2b84bf7b48ee
cedric thieulot <c.thieulot@uu.nl>
2026-09-14 14:13:48 +0200

    f65cleanup

--- python_codes/fieldstone_65/stone.py
+++ python_codes/fieldstone_65/stone.py
@@ -1,24 +1,20 @@
 import numpy as np
-import sys as sys
-import scipy
+import time as clock
 import scipy.sparse as sps
-from scipy.sparse.linalg.dsolve import linsolve
-import time as timing
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-#------------------------------------------------------------------------------
+###############################################################################
 
 sqrt3=np.sqrt(3.)
 eps=1.e-10 
 
-print("-----------------------------")
-print("----------fieldstone---------")
-print("-----------------------------")
+print("*******************************")
+print("********** stone 065 **********")
+print("*******************************")
 
-ndim=2       # number of space dimensions
-m=4          # number of nodes making up an element
-ndofT=1      # number of degrees of freedom per node
+ndim=2 # number of space dimensions
+m=4    # number of nodes making up an element
 
 # exp 1: 1D problem
 # exp 2: 2D skew to the mesh
@@ -35,7 +31,7 @@ if experiment==1:
    hx=Lx/float(nelx)
    hy=Ly/float(nely)
    f=1.
-   Pe=1
+   Pe=5
    u0=1.
    hcapa=1.     # heat capacity
    rho0=1       # reference density
@@ -78,12 +74,11 @@ if experiment==4:
    hx=Lx/float(nelx)
    hy=Ly/float(nely)
    f=0.
-   Pe=10
+   Pe=1
    u0=1.
    hcapa=1.     # heat capacity
    rho0=1       # reference density
    hcond=u0*hx*rho0*hcapa/2/Pe     # thermal conductivity
-
 
 use_artificial_diffusion=False
 
@@ -91,11 +86,11 @@ use_supg=False
 
 kappa=hcond/rho0/hcapa
 
-nnx=nelx+1  # number of elements, x direction
-nny=nely+1  # number of elements, y direction
-NV=nnx*nny  # number of nodes
-nel=nelx*nely  # number of elements, total
-NfemT=NV*ndofT  # Total number of degrees of temperature freedom
+nnx=nelx+1    # number of elements, x direction
+nny=nely+1    # number of elements, y direction
+nn_T=nnx*nny  # number of nodes
+nel=nelx*nely # number of elements, total
+NfemT=nn_T    # Total number of degrees of temperature freedom
 
 #####################################################################
 
@@ -135,15 +130,15 @@ print("kappa=",hcond/rho0/hcapa)
 print("Pe=",Pe)
 print("beta=",beta)
 print("kappatilde=",kappatilde)
+print("*******************************")
 
-#####################################################################
+###############################################################################
 # grid point setup 
-#####################################################################
+###############################################################################
+start=clock.time()
 
-print("grid point setup")
-
-x = np.empty(NV,dtype=np.float64)  # x coordinates
-y = np.empty(NV,dtype=np.float64)  # y coordinates
+x=np.zeros(nn_T,dtype=np.float64)  # x coordinates
+y=np.zeros(nn_T,dtype=np.float64)  # y coordinates
 
 counter = 0
 for j in range(0, nny):
@@ -154,12 +149,15 @@ for j in range(0, nny):
     #end for
 #end for
 
-#####################################################################
-# velocity field 
-#####################################################################
+print("node coordinates: %.3f s" % (clock.time()-start))
 
-u = np.zeros(NV,dtype=np.float64)  # x-component velocity
-v = np.zeros(NV,dtype=np.float64)  # y-component velocity
+###############################################################################
+# velocity field 
+###############################################################################
+start=clock.time()
+
+u=np.zeros(nn_T,dtype=np.float64)  # x-component velocity
+v=np.zeros(nn_T,dtype=np.float64)  # y-component velocity
 
 if experiment==1 or experiment==3:
    counter = 0
@@ -197,44 +195,45 @@ if experiment==4:
        #end for
    #end for
 
+print("prescribe vel field: %.3f s" % (clock.time()-start))
 
-#####################################################################
+###############################################################################
 # connectivity
-#####################################################################
+###############################################################################
+start=clock.time()
 
-print("connectivity array")
+icon=np.zeros((m,nel),dtype=np.int32)
 
-icon =np.zeros((m, nel),dtype=np.int32)
-counter = 0
-for j in range(0, nely):
-    for i in range(0, nelx):
-        icon[0, counter] = i + j * (nelx + 1)
-        icon[1, counter] = i + 1 + j * (nelx + 1)
-        icon[2, counter] = i + 1 + (j + 1) * (nelx + 1)
-        icon[3, counter] = i + (j + 1) * (nelx + 1)
-        counter += 1
+counter=0
+for j in range(0,nely):
+    for i in range(0,nelx):
+        icon[0,counter] = i + j * (nelx + 1)
+        icon[1,counter] = i + 1 + j * (nelx + 1)
+        icon[2,counter] = i + 1 + (j + 1) * (nelx + 1)
+        icon[3,counter] = i + (j + 1) * (nelx + 1)
+        counter+=1
     #end for
 #end for
+
+print("build connectivity: %.3f s" % (clock.time()-start))
 
 #####################################################################
 # define temperature boundary conditions
 #####################################################################
-
-print("defining temperature boundary conditions")
+start=clock.time()
 
 bc_fixT=np.zeros(NfemT,dtype=bool)  
 bc_valT=np.zeros(NfemT,dtype=np.float64) 
 
 if experiment==1:
-   for i in range(0,NV):
+   for i in range(0,nn_T):
        if x[i]/Lx<eps:
           bc_fixT[i]=True ; bc_valT[i]=0.
        if x[i]/Lx>(1-eps):
           bc_fixT[i]=True ; bc_valT[i]=0.
-   #end for
 
 if experiment==2:
-   for i in range(0,NV):
+   for i in range(0,nn_T):
        if x[i]/Lx<eps and y[i]<0.2:
           bc_fixT[i]=True ; bc_valT[i]=0.
        if x[i]/Lx<eps and y[i]>=0.2:
@@ -243,7 +242,7 @@ if experiment==2:
           bc_fixT[i]=True ; bc_valT[i]=0.
 
 if experiment==3:
-   for i in range(0,NV):
+   for i in range(0,nn_T):
        if x[i]/Lx<eps:
           if y[i]>0.5:
              bc_fixT[i]=True ; bc_valT[i]=0.
@@ -255,10 +254,9 @@ if experiment==3:
           bc_fixT[i]=True ; bc_valT[i]=0.
        #if y[i]/Ly>(1-eps):
        #   bc_fixT[i]=True ; bc_valT[i]=0.
-   #end for
 
 if experiment==4:
-   for i in range(0,NV):
+   for i in range(0,nn_T):
        if y[i]/Ly<eps:
           bc_fixT[i]=True ; bc_valT[i]=0.
        if y[i]/Ly>(1-eps):
@@ -266,39 +264,41 @@ if experiment==4:
        if (x[i]-2)**2+(y[i]-2)**2<1:
           bc_fixT[i]=True ; bc_valT[i]=1.
 
-   #end for
+print("initial temperature: %.3f s" % (clock.time()-start))
 
-#####################################################################
+###############################################################################
 # compute analytical solution
-#####################################################################
+###############################################################################
+start=clock.time()
 
-T_anal  = np.zeros(NV,dtype=np.float64)
+T_anal=np.zeros(nn_T,dtype=np.float64)
 
 if experiment==1:
-   for i in range(0,NV):
+   for i in range(0,nn_T):
        T_anal[i]=(x[i]-(1-np.exp(2*Pe*x[i]/hx))/(1-np.exp(2*Pe/hx)))/u0*f
+
 if experiment==2 or experiment==3 or experiment==4:
    T_anal[:]=0.
 
-#####################################################################
-# create necessary arrays 
-#####################################################################
+###############################################################################
+# build linear system 
+###############################################################################
+start=clock.time()
 
-T     = np.zeros(NV,dtype=np.float64)
-N     = np.zeros(m,dtype=np.float64)    # shape functions
-dNdx  = np.zeros(m,dtype=np.float64)    # shape functions derivatives
-dNdy  = np.zeros(m,dtype=np.float64)    # shape functions derivatives
-dNdr  = np.zeros(m,dtype=np.float64)    # shape functions derivatives
-dNds  = np.zeros(m,dtype=np.float64)    # shape functions derivatives
+N     = np.zeros(m,dtype=np.float64) # shape functions
+dNdx  = np.zeros(m,dtype=np.float64) # shape functions derivatives
+dNdy  = np.zeros(m,dtype=np.float64) # shape functions derivatives
+dNdr  = np.zeros(m,dtype=np.float64) # shape functions derivatives
+dNds  = np.zeros(m,dtype=np.float64) # shape functions derivatives
 A_mat = np.zeros((NfemT,NfemT),dtype=np.float64) # FE matrix 
 rhs   = np.zeros(NfemT,dtype=np.float64)         # FE rhs 
-B_mat = np.zeros((2,ndofT*m),dtype=np.float64)     # gradient matrix B 
+B_mat = np.zeros((2,m),dtype=np.float64)         # gradient matrix B 
 N_mat = np.zeros((m,1),dtype=np.float64)         # shape functions
 
 for iel in range (0,nel):
 
-    b_el=np.zeros(m*ndofT,dtype=np.float64)
-    a_el=np.zeros((m*ndofT,m*ndofT),dtype=np.float64)
+    b_el=np.zeros(m,dtype=np.float64)
+    A_el=np.zeros((m,m),dtype=np.float64)
     Ka=np.zeros((m,m),dtype=np.float64)   # elemental advection matrix 
     Kd=np.zeros((m,m),dtype=np.float64)   # elemental diffusion matrix 
     vel=np.zeros((1,ndim),dtype=np.float64)
@@ -311,7 +311,7 @@ for iel in range (0,nel):
             sq=jq/sqrt3
             weightq=1.*1.
 
-            # calculate shape functions
+            # calculate Q1 shape functions
             N_mat[0,0]=0.25*(1.-rq)*(1.-sq)
             N_mat[1,0]=0.25*(1.+rq)*(1.-sq)
             N_mat[2,0]=0.25*(1.+rq)*(1.+sq)
@@ -330,7 +330,6 @@ for iel in range (0,nel):
                 jcb[0,1]+=dNdr[k]*y[icon[k,iel]]
                 jcb[1,0]+=dNds[k]*x[icon[k,iel]]
                 jcb[1,1]+=dNds[k]*y[icon[k,iel]]
-            # end for
 
             # calculate the determinant of the jacobian
             jcob=np.linalg.det(jcb)
@@ -348,132 +347,137 @@ for iel in range (0,nel):
                 dNdy[k]=jcbi[1,0]*dNdr[k]+jcbi[1,1]*dNds[k]
                 B_mat[0,k]=dNdx[k]
                 B_mat[1,k]=dNdy[k]
-                b_el[k]=N_mat[k,0]*f*jcob*weightq
+                b_el[k]+=N_mat[k,0]*f*jcob*weightq
             # end for
 
             # compute diffusion matrix
-            Kd=B_mat.T.dot(B_mat)*(hcond+hcondtilde)*weightq*jcob
+            Kd+=B_mat.T.dot(B_mat)*(hcond+hcondtilde)*weightq*jcob
 
             if use_supg:
                tau_supg=0.5*hx/u0
                N_mat+=tau_supg*np.transpose(vel.dot(B_mat))
 
             # compute advection matrix
-            Ka=N_mat.dot(vel.dot(B_mat))*rho0*hcapa*weightq*jcob
-
-            a_el=Ka+Kd
-
-            # apply boundary conditions
-            for k1 in range(0,m):
-                m1=icon[k1,iel]
-                if bc_fixT[m1]:
-                   Aref=a_el[k1,k1]
-                   for k2 in range(0,m):
-                       m2=icon[k2,iel]
-                       b_el[k2]-=a_el[k2,k1]*bc_valT[m1]
-                       a_el[k1,k2]=0
-                       a_el[k2,k1]=0
-                   a_el[k1,k1]=Aref
-                   b_el[k1]=Aref*bc_valT[m1]
-                #end if
-            #end for
-
-            # assemble matrix A_mat and right hand side rhs
-            for k1 in range(0,m):
-                m1=icon[k1,iel]
-                for k2 in range(0,m):
-                    m2=icon[k2,iel]
-                    A_mat[m1,m2]+=a_el[k1,k2]
-                #end for
-                rhs[m1]+=b_el[k1]
-            #end for
+            Ka+=N_mat.dot(vel.dot(B_mat))*rho0*hcapa*weightq*jcob
 
         #end for jq
     #end for iq
+
+    A_el=Ka+Kd
+
+    # apply boundary conditions
+    for k1 in range(0,m):
+        m1=icon[k1,iel]
+        if bc_fixT[m1]:
+           Aref=A_el[k1,k1]
+           for k2 in range(0,m):
+               m2=icon[k2,iel]
+               b_el[k2]-=A_el[k2,k1]*bc_valT[m1]
+               A_el[k1,k2]=0
+               A_el[k2,k1]=0
+           A_el[k1,k1]=Aref
+           b_el[k1]=Aref*bc_valT[m1]
+        #end if
+    #end for
+
+    # assemble matrix A_mat and right hand side rhs
+    for k1 in range(0,m):
+        m1=icon[k1,iel]
+        for k2 in range(0,m):
+            m2=icon[k2,iel]
+            A_mat[m1,m2]+=A_el[k1,k2]
+        #end for
+        rhs[m1]+=b_el[k1]
+    #end for
+
 #end for iel
 
-#################################################################
+print("build system: %.3f s" % (clock.time()-start))
+
+###############################################################################
 # solve system
-#################################################################
-start = timing.time()
+###############################################################################
+start=clock.time()
 
 T = sps.linalg.spsolve(sps.csr_matrix(A_mat),rhs)
-print("solve T time: %.3f s" % (timing.time() - start))
 
 print("T (m,M) %.4f %.4f " %(np.min(T),np.max(T)))
 
 np.savetxt('Temperature.ascii',np.array([x,y,T,T_anal]).T,header='# x,y,T')
 
-#################################################################
+print("solve T time: %.3f s" % (clock.time()-start))
+
+###############################################################################
 # visualisation 
-#################################################################
+###############################################################################
+start=clock.time()
 
-visu=1
-
-if visu==1:
-    vtufile=open('solution.vtu',"w")
-    vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
-    vtufile.write("<UnstructuredGrid> \n")
-    vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(NV,nel))
-    #####
-    vtufile.write("<Points> \n")
-    vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
-    for i in range(0,NV):
+vtufile=open('solution.vtu',"w")
+vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+vtufile.write("<UnstructuredGrid> \n")
+vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(nn_T,nel))
+#####
+vtufile.write("<Points> \n")
+vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+for i in range(0,nn_T):
         vtufile.write("%10f %10f %10f \n" %(x[i],y[i],0.))
-    vtufile.write("</DataArray>\n")
-    vtufile.write("</Points> \n")
-    #####
-    vtufile.write("<PointData Scalars='scalars'>\n")
-    #--
-    vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Name='velocity' Format='ascii'> \n")
-    for i in range(0,NV):
+vtufile.write("</DataArray>\n")
+vtufile.write("</Points> \n")
+#####
+vtufile.write("<PointData Scalars='scalars'>\n")
+#--
+vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Name='velocity' Format='ascii'> \n")
+for i in range(0,nn_T):
         vtufile.write("%10f %10f %10f \n" %(u[i],v[i],0.))
-    vtufile.write("</DataArray>\n")
-    #--
-    vtufile.write("<DataArray type='Float32' Name='temperature' Format='ascii'> \n")
-    for i in range(0,NV):
-        vtufile.write("%10f \n" %T[i])
-    vtufile.write("</DataArray>\n")
-    #--
-    vtufile.write("</PointData>\n")
-    #####
-    vtufile.write("<Cells>\n")
-    #--
-    vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
-    for iel in range (0,nel):
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("<DataArray type='Float32' Name='temperature' Format='ascii'> \n")
+T.tofile(vtufile,sep=' ',format='%.4e')
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("</PointData>\n")
+#####
+vtufile.write("<Cells>\n")
+#--
+vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+for iel in range (0,nel):
         vtufile.write("%d %d %d %d\n" %(icon[0,iel],icon[1,iel],icon[2,iel],icon[3,iel]))
-    vtufile.write("</DataArray>\n")
-    #--
-    vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
-    for iel in range (0,nel):
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+for iel in range (0,nel):
         vtufile.write("%d \n" %((iel+1)*4))
-    vtufile.write("</DataArray>\n")
-    #--
-    vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
-    for iel in range (0,nel):
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+for iel in range (0,nel):
         vtufile.write("%d \n" %9)
-    vtufile.write("</DataArray>\n")
-    #--
-    vtufile.write("</Cells>\n")
-    #####
-    vtufile.write("</Piece>\n")
-    vtufile.write("</UnstructuredGrid>\n")
-    vtufile.write("</VTKFile>\n")
-    vtufile.close()
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("</Cells>\n")
+#####
+vtufile.write("</Piece>\n")
+vtufile.write("</UnstructuredGrid>\n")
+vtufile.write("</VTKFile>\n")
+vtufile.close()
 
-    filename = 'solution.pdf'
-    fig = plt.figure ()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(x.reshape ((nny,nnx)),y.reshape((nny,nnx)),T.reshape((nny,nnx)),color = 'darkseagreen')
-    ax.set_xlabel ( 'X')
-    ax.set_ylabel ( 'Y')
-    ax.set_zlabel ( ' Temperature')
-    plt.title('Peclet Nb: %e' %(Pe),loc='right')
-    plt.grid ()
-    plt.savefig(filename)
-    #plt.show ()
-    plt.close()
+filename = 'solution.pdf'
+fig = plt.figure ()
+ax = plt.axes(projection='3d')
+ax.plot_surface(x.reshape ((nny,nnx)),y.reshape((nny,nnx)),T.reshape((nny,nnx)),color = 'darkseagreen')
+ax.set_xlabel ( 'X')
+ax.set_ylabel ( 'Y')
+ax.set_zlabel ( ' Temperature')
+plt.title('Peclet Nb: %e' %(Pe),loc='right')
+plt.grid ()
+plt.savefig(filename)
+plt.show ()
+plt.close()
+
+print("export to vtu: %.3f s" % (clock.time()-start))
     
-print("-----------------------------")
-print("------------the end----------")
-print("-----------------------------")
+print("*******************************")
+print("********** the end ************")
+print("*******************************")
+
+###############################################################################
