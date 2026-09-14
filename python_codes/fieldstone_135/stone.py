18caeb7b11ce62c5685e292c0a06307ccf55f286
cedric thieulot <c.thieulot@uu.nl>
2026-09-14 16:15:08 +0200

    f135updt

--- python_codes/fieldstone_135/stone.py
+++ python_codes/fieldstone_135/stone.py
@@ -4,21 +4,22 @@ import numpy as np
 slabid=np.zeros(1500000)
 
 ###############################################################################
+# 'slab2/jap_09-21_input.csv',\ it seems to have disappeared from database ?!
 
 count_total=0
 
 rows = []
 slab_id=0
-for name in 'slab2/alu_09-21_input.csv', 'slab2/cal_09-21_input.csv', 'slab2/cam_09-21_input.csv',\
-            'slab2/car_09-21_input.csv', 'slab2/cas_09-21_input.csv', 'slab2/cot_09-21_input.csv',\
-            'slab2/hal_09-21_input.csv', 'slab2/hel_09-21_input.csv', 'slab2/him_09-21_input.csv',\
-            'slab2/hin_09-21_input.csv', 'slab2/izu_09-21_input.csv', 'slab2/jap_09-21_input.csv',\
-            'slab2/ker_09-21_input.csv', 'slab2/kur_09-21_input.csv', 'slab2/mak_09-21_input.csv',\
-            'slab2/man_09-21_input.csv', 'slab2/mue_09-21_input.csv', 'slab2/pam_09-21_input.csv',\
-            'slab2/phi_09-21_input.csv', 'slab2/png_09-21_input.csv', 'slab2/puy_09-21_input.csv',\
-            'slab2/ryu_09-21_input.csv', 'slab2/sam_09-21_input.csv', 'slab2/sco_09-21_input.csv',\
-            'slab2/sol_09-21_input.csv', 'slab2/sul_09-21_input.csv', 'slab2/sum_09-21_input.csv',\
-            'slab2/van_09-21_input.csv':
+for name in 'slab2/alu_06-23_input.csv', 'slab2/cal_06-23_input.csv', 'slab2/cam_06-23_input.csv',\
+            'slab2/car_06-23_input.csv', 'slab2/cas_06-23_input.csv', 'slab2/cot_06-23_input.csv',\
+            'slab2/hal_06-23_input.csv', 'slab2/hel_06-23_input.csv', 'slab2/him_06-23_input.csv',\
+            'slab2/hin_06-23_input.csv', 'slab2/izu_06-23_input.csv',\
+            'slab2/ker_06-23_input.csv', 'slab2/kur_06-23_input.csv', 'slab2/mak_06-23_input.csv',\
+            'slab2/man_06-23_input.csv', 'slab2/mue_06-23_input.csv', 'slab2/pam_06-23_input.csv',\
+            'slab2/phi_06-23_input.csv', 'slab2/png_06-23_input.csv', 'slab2/puy_06-23_input.csv',\
+            'slab2/ryu_06-23_input.csv', 'slab2/sam_06-23_input.csv', 'slab2/sco_06-23_input.csv',\
+            'slab2/sol_06-23_input.csv', 'slab2/sul_06-23_input.csv', 'slab2/sum_06-23_input.csv',\
+            'slab2/van_06-23_input.csv':
 
     with open(name) as csv_file:
          csv_reader = csv.reader(csv_file, delimiter=',')
@@ -66,52 +67,52 @@ C_x=C_r*np.sin(C_theta)*np.cos(C_phi)
 C_y=C_r*np.sin(C_theta)*np.sin(C_phi)
 C_z=C_r*np.cos(C_theta)
 
-if True:
-   vtufile=open("slab2.vtu","w")
-   vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
-   vtufile.write("<UnstructuredGrid> \n")
-   vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(N,N))
-   #####
-   vtufile.write("<Points> \n")
-   vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
-   for i in range(0,N):
-       vtufile.write("%10f %10f %10f \n" %(C_x[i],C_y[i],C_z[i]))
-   vtufile.write("</DataArray>\n")
-   vtufile.write("</Points> \n")
-   #####
-   vtufile.write("<PointData>\n")
-   #--
-   vtufile.write("<DataArray type='Float32' Name='depth' Format='ascii'> \n")
-   for i in range(0,N):
-       vtufile.write("%10e \n" %(depth[i]))
-   vtufile.write("</DataArray>\n")
-   #--
-   vtufile.write("<DataArray type='Float32' Name='id' Format='ascii'> \n")
-   for i in range(0,N):
-       vtufile.write("%10e \n" %(slabid[i]))
-   vtufile.write("</DataArray>\n")
+vtufile=open("slab2.vtu","w")
+vtufile.write("<VTKFile type='UnstructuredGrid' version='0.1' byte_order='BigEndian'> \n")
+vtufile.write("<UnstructuredGrid> \n")
+vtufile.write("<Piece NumberOfPoints=' %5d ' NumberOfCells=' %5d '> \n" %(N,N))
+#####
+vtufile.write("<Points> \n")
+vtufile.write("<DataArray type='Float32' NumberOfComponents='3' Format='ascii'> \n")
+for i in range(0,N):
+    vtufile.write("%10f %10f %10f \n" %(C_x[i],C_y[i],C_z[i]))
+vtufile.write("</DataArray>\n")
+vtufile.write("</Points> \n")
+#####
+vtufile.write("<PointData>\n")
+#--
+vtufile.write("<DataArray type='Float32' Name='depth' Format='ascii'> \n")
+depth.tofile(vtufile,sep=' ',format='%.4e')
+#for i in range(0,N):
+#    vtufile.write("%10e \n" %(depth[i]))
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("<DataArray type='Float32' Name='id' Format='ascii'> \n")
+slabid.tofile(vtufile,sep=' ',format='%.4e')
+#for i in range(0,N):
+#    vtufile.write("%10e \n" %(slabid[i]))
+vtufile.write("</DataArray>\n")
+#--
+vtufile.write("</PointData>\n")
+#####
+vtufile.write("<Cells>\n")
+vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
+for i in range (0,N):
+    vtufile.write("%d \n" % i)
+vtufile.write("</DataArray>\n")
+vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
+for i in range (0,N):
+    vtufile.write("%d \n" %(i+1))
+vtufile.write("</DataArray>\n")
+vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
+for iel in range (0,N):
+    vtufile.write("%d \n" % 1)
+vtufile.write("</DataArray>\n")
+vtufile.write("</Cells>\n")
+#####
+vtufile.write("</Piece>\n")
+vtufile.write("</UnstructuredGrid>\n")
+vtufile.write("</VTKFile>\n")
+vtufile.close()
 
-
-   #--
-   vtufile.write("</PointData>\n")
-   #####
-   vtufile.write("<Cells>\n")
-   vtufile.write("<DataArray type='Int32' Name='connectivity' Format='ascii'> \n")
-   for i in range (0,N):
-       vtufile.write("%d \n" % i)
-   vtufile.write("</DataArray>\n")
-   vtufile.write("<DataArray type='Int32' Name='offsets' Format='ascii'> \n")
-   for i in range (0,N):
-       vtufile.write("%d \n" %(i+1))
-   vtufile.write("</DataArray>\n")
-   vtufile.write("<DataArray type='Int32' Name='types' Format='ascii'>\n")
-   for iel in range (0,N):
-       vtufile.write("%d \n" % 1)
-   vtufile.write("</DataArray>\n")
-   vtufile.write("</Cells>\n")
-   #####
-   vtufile.write("</Piece>\n")
-   vtufile.write("</UnstructuredGrid>\n")
-   vtufile.write("</VTKFile>\n")
-   vtufile.close()
-
+###############################################################################
